de3c3dc84f42410cf5124011a8e7b66bda7860e6
MusaTRGG <musababacs@gmail.com>
2026-06-11 00:14:08 +0300

    yeteeeeer

--- frontend/src/supabaseConfig.js
+++ frontend/src/supabaseConfig.js
@@ -2,4 +2,4 @@
 // Lütfen Supabase panelinden (Project Settings -> API) aldığınız değerleri buraya yapıştırın.
 
 export const SUPABASE_URL = "https://chtvnyqqcqxndlxtljok.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_7-5vZ3M09zLI79y5sjyuYw_FdRP4Uuf";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNodHZueXFxY3F4bmRseHRsam9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExMDk2ODUsImV4cCI6MjA5NjY4NTY4NX0.lE1ksEaeJcFbQQQqliW1TZ3xJdwtd64J7us0UerBxKE";
